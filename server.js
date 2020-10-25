773bfa6dfdc27fd9089dd0d0d29d3d80d9320f6d
Jwickster <joshwickster@gmail.com>
2020-10-25 14:35:46 -0600

    edit server.js

--- server.js
+++ server.js
@@ -8,8 +8,19 @@ app.get("/", function (req, res) {
     //This will show on browser
     res.send("<h1>Hello from response</h1>");
     //console.log(request);
-
 });
+
+app.get("/contact", function (req, res) {
+    res.send("Contact me at joshwickster@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+
+    res.send("Software engineer in Salt Lake City Utah");
+});
+
+
+
 
 
 //set up on localhost port 3001
