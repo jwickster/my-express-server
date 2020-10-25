3acad64ffca5d46f4fa8d19f1038e6633e733081
Jwickster <joshwickster@gmail.com>
2020-10-25 14:22:17 -0600

    add comments

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+//jshint esversion:6
+
 const express = require("express");
 
 const app = express();
 
+app.get("/", function (request, response) {
+    //This will show on browser
+    response.send("Hello from response");
+    //console.log(request);
+
+})
+
+
 //set up on localhost 3000
-app.listen(3000, function () {
+app.listen(3001, function () {
     console.log("Server listening on port 3000");
 });
